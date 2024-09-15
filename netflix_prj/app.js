fc32aa8e123513f5d68b455b8c92d3d9ce05462f
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-09-15 12:08:07 +0530

    styles added to landing page

---
+++ netflix_prj/app.js
@@ -0,0 +1,33 @@
+let movies=[
+    {
+        name:"Stranger things",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyKu0FsmjKVMIisF2ALtMaANdeCJWaNBbnlA&s",
+        rating:10
+    },
+    {
+        name:"Jumanji",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXJALX71JwANVw87_6b1RhTwDBQ5tvuJxg4w&s",
+    },
+    {
+        name:"Venom",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2-0oNe5A4hmea4ZmQ92OHBfEwQzkqxk3I9A&s",
+        rating:7.0
+    },
+    {
+        name:"DeadPool",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwPTU59Y8zBkKd5MFStMQcIqFXb4Ne4XUb9Q&s",
+        rating:9.0
+    },
+    {
+        name:"Titanic",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT20zO6zLZKX3wdhHtvImR97spXXqt3mzuiVQ&s",
+        rating:7.5
+    }
+
+]
+function searchmovie()
+{
+    var inp=document.querySelector(".search-box").value;
+
+    console.log( inp)
+}
