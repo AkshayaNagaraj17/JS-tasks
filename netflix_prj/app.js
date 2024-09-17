4bb43a3dd6654ab101e6d514e3b9f646d39df269
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-09-17 10:08:08 +0530

    innerhtml added

--- netflix_prj/app.js
+++ netflix_prj/app.js
@@ -31,3 +31,40 @@ function searchmovie()
 
     console.log( inp)
 }
+
+function displayMovie()
+{
+    // let moviediv=document.createElement("div")
+    // moviediv.classList.add("movie")
+    // let overlaydiv=document.createElement("div")
+    // overlaydiv.classList.add("overlay")
+    // moviediv.appendChild(overlaydiv)  //letting overlay inside the movie div
+    // console.log(moviediv)
+
+
+    //another way
+
+    let htmlString=`
+    
+    <div class="movie">
+            <div class="overlay">
+                 <div class="video">
+
+                 </div>
+                 <div class="details">
+                    <h1>Stranger things</h1>
+                    <h2>IMDB:8.5</h2>
+                    <p>Eleven . Sid . Demon</p>
+                 </div>
+            </div>
+            <img class="ST" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyKu0FsmjKVMIisF2ALtMaANdeCJWaNBbnlA&s" alt="ST poster">
+        </div>
+    
+    
+    `
+    document.querySelector(".movies").innerHTML=htmlString
+
+
+}
+
+displayMovie()
