da02c3616901c59146a5e6f664c7649f3dd1688f
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-09-23 19:10:23 +0530

    OTP VERFICATION FOCUS DONE

--- OTP-verification/app.js
+++ OTP-verification/app.js
@@ -1,12 +1,11 @@
-// Select all input elements
-let inputs = document.querySelectorAll("input");
+let inputs=document.querySelectorAll("input")
 
-inputs.forEach((input, index) => {
-    input.addEventListener('keyup', () => {
-        // Check if the current input is not the last one
-        if (index < inputs.length - 1) {
-            // Move focus to the next input
-            inputs[index + 1].focus();
+inputs.forEach((input,index) =>
+    {
+    input.addEventListener("keyup",()=>
+    {
+        if(index<inputs.length-1){
+            inputs[index+1].focus()
         }
-    });
-});
+    })
+})
